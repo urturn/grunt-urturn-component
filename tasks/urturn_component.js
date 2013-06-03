b05f91a84306c3325cb3c687f51835f70d9b35a7
Olivier Amblet <olivier.amblet@webdoc.com>
2013-06-03 16:19:05 +0200

    added grunt_component_install task

--- tasks/urturn_component.js
+++ tasks/urturn_component.js
@@ -29,4 +29,80 @@ module.exports = function(grunt) {
     grunt.log.writeln("component.urturn.json written");
   });
 
+  grunt.registerTask('urturn_component_install', "Install urturn component.", function() {
+    var bower = require('bower');
+    var path = require('path');
+    var options = this.options({
+      dest: "lib"
+    });
+
+    var deps = {};
+    function Component (data){
+      this.name = data.name;
+      this.version = data.version || "0.0.0";
+      if(data.main){
+        (typeof data.main === 'string' ? this.main = [data.main] : this.main = data.main);
+      } else {
+        data.main = [];
+      }
+      this.basedir = data.basedir || path.join(bower.config.directory, this.name);
+      this.assets = data.assets || [];
+      this.files = function(){
+        return this.main.concat(this.assets);
+      };
+
+      this.dependencies = {};
+
+      if (data.dependencies) {
+        for (var dep in data.dependencies) {
+          var depPath = path.join(bower.config.directory, dep);
+          var compPath = path.join(depPath, 'component.urturn.json');
+          var bowerPath = path.join(depPath, 'bower.json');
+          var legacyBowerPath = path.join(depPath, 'component.json');
+          if( grunt.file.exists(compPath)) {
+            this.dependencies[dep] = Component.fromOptions(grunt.file.readJSON(compPath));
+          } else if(grunt.file.exists(bowerPath)){
+            this.dependencies[dep] = Component.fromBower(dep, grunt.file.readJSON(bowerPath));
+          } else if (grunt.file.exists(legacyBowerPath)){
+            this.dependencies[dep] = Component.fromBower(dep, grunt.file.readJSON(legacyBowerPath));
+          } else {
+            grunt.log.error(depPath, "No json descriptor found for " + dep);
+          }
+        }
+      }
+    }
+    Component.fromOptions = function(info){
+      if(!Component.dict[info.name]){
+        Component.dict[info.name] = new Component(info);
+        Component.list.push(Component.dict[info.name]);
+      }
+      return Component.dict[info.name];
+    };
+    Component.fromBower = function(name, info){
+      if(!Component.dict[name]){
+        Component.dict[name] = new Component({
+          name: info.name,
+          version: info.version,
+          dependencies: info.dependencies || {},
+          main: info.main
+        });
+        Component.list.push(Component.dict[info.name]);
+      }
+      return Component.dict[name];
+    };
+    Component.list = [];
+    Component.dict = {};
+
+
+    var info = grunt.file.readJSON('component.urturn.json');
+    info.basedir = '.';
+    Component.fromOptions(info);
+    Component.list.forEach(function(comp){
+      comp.files().forEach(function(f){
+        grunt.file.copy( path.join(comp.basedir, f), path.join(options.dest, comp.name, f));
+      });
+    });
+    console.log('info', info);
+  });
+
 };
